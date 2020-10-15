bf043c629f6e2ed0f37c955fb1b48029bdd29f87
sam <petits@tcd.ie>
2020-10-15 11:06:37 +0200

    finshing weekly 2

--- weekly/2/code/Qc.py
+++ weekly/2/code/Qc.py
@@ -16,9 +16,9 @@ X1_square = X1.reshape(-1, 1)
 X2_square = X2.reshape(-1, 1)
 X1_square = np.square(X1)
 X2_square = np.square(X2)
-
 X = np.column_stack((X1, X2, X1_square, X2_square))
 
+# Train our model
 model = LogisticRegression(solver='lbfgs', penalty="none")
 model.fit(X, ytrain)
 
@@ -32,10 +32,20 @@ ypred = np.sign(model.predict(X))
 
 # Plot the preditions
 plt.rc('font', size=20)
-pos = plt.scatter(X1[ypred > 0], X2[ypred > 0],
-                  color='red', marker="+")
-pos = plt.scatter(X1[ypred < 0], X2[ypred < 0],
-                  color='blue', marker=".")
-
+pos = plt.scatter(X1[y > 0], X2[y > 0],
+                  color='black', marker=".")
+neg = plt.scatter(X1[y < 0], X2[y < 0],
+                  color='green', marker=".")
+pos_pred = plt.scatter(X1[ypred > 0], X2[ypred > 0],
+                       color='red', marker="+")
+neg_pred = plt.scatter(X1[ypred < 0], X2[ypred < 0],
+                       color='blue', marker="+")
 plt.rcParams['figure.constrained_layout.use'] = True
+plt.xlabel("X1")
+plt.ylabel("X2")
+plt.legend((neg, pos, pos_pred, neg_pred), ["positive", "negative", "positive predictions", "negative predictions"],
+           scatterpoints=1,
+           loc='upper right',
+           ncol=2,
+           fontsize=18)
 plt.show()
