d1cd00ba9f8414585852bd7d55d8cb04492a3e68
sam <petits@tcd.ie>
2020-10-21 11:23:08 +0200

    finishing code for week 3 ML

--- weekly/3/code/question_2.py
+++ weekly/3/code/question_2.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import statistics
+from sklearn.linear_model import Ridge
 
 # Read in data
 df = pd.read_csv("week3.csv", comment='#')
@@ -48,4 +49,63 @@ ax.set_xlabel("number of folds")
 ax.set_title(
     "Variance and Mean against number of folds used. Lasso Model with C = 1")
 ax.legend(loc='lower right')
+
+means = []
+variances = []
+Cs = []
+num_folds = 10
+C_range = np.arange(1, 75, 5)
+for C in C_range:
+    kf = KFold(n_splits=num_folds)
+    mse = []
+    for train, test in kf.split(X_poly):
+        model = linear_model.Lasso(alpha=(1/C))
+        model.fit(X_poly[train], y[train])
+        ypred = model.predict(X_poly[test])
+        mse.append(mean_squared_error(y[test], ypred))
+    means.append(statistics.mean(mse))
+    variances.append(statistics.variance(mse))
+
+# Plot the means and variances
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.errorbar(C_range, means, label='Mean',
+            yerr=C_range, uplims=True, lolims=True)
+ax.errorbar(C_range, variances, label='Variance',
+            yerr=C_range, uplims=True, lolims=True)
+ax.set_xlabel("mean/variance")
+ax.set_xlabel("C")
+ax.set_title(
+    "Variance and Mean against C value. Lasso Model with folds=10")
+ax.legend(loc='lower right')
+
+means = []
+variances = []
+Cs = []
+num_folds = 10
+for C in C_range:
+    kf = KFold(n_splits=num_folds)
+    mse = []
+    for train, test in kf.split(X_poly):
+        model = Ridge(alpha=(1/(2 * C)))
+        model.fit(X_poly[train], y[train])
+        ypred = model.predict(X_poly[test])
+        mse.append(mean_squared_error(y[test], ypred))
+    means.append(statistics.mean(mse))
+    variances.append(statistics.variance(mse))
+
+# Plot the means and variances
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.errorbar(C_range, means, label='Mean',
+            yerr=C_range, uplims=True, lolims=True)
+ax.errorbar(C_range, variances, label='Variance',
+            yerr=C_range, uplims=True, lolims=True)
+ax.set_xlabel("mean/variance")
+ax.set_xlabel("C")
+ax.set_title(
+    "Variance and Mean against C value. Ridge Model with folds=10")
+ax.legend(loc='lower right')
+
+
 plt.show()
