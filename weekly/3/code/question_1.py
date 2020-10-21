5d49c2cab555c44ef37b13df9a55ed524913e856
sam <petits@tcd.ie>
2020-10-21 11:42:41 +0200

    cleaning code for w3

--- weekly/3/code/question_1.py
+++ weekly/3/code/question_1.py
@@ -1,5 +1,4 @@
 from sklearn.linear_model import Ridge
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -13,7 +12,7 @@ X2 = df.iloc[:, 1]
 X = np.column_stack((X1, X2))
 y = np.array(df.iloc[:, 2])
 
-# Plot Q1 a 3D figure.
+# Plot Q1 a - 3D figure.
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(X[:, 0], X[:, 1], y)
@@ -23,14 +22,16 @@ ax.set_zlabel('Y', color='blue', size=15)
 ax.set_title("3D Plot of training data as scatter")
 
 # Q1 b training lasso models
+# Grab the powers up to 5.
 poly = PolynomialFeatures(5)
 X_poly = poly.fit_transform(X)
-
+# Generate the [-5,5] grid for predictions.
 Xtest = []
 grid = np.linspace(-5, 5)
 for i in grid:
     for j in grid:
         Xtest.append([i, j])
+# Grab the powers up to 5 for the grid data.
 Xtest = np.array(Xtest)
 Xtest_poly = poly.fit_transform(Xtest)
 
@@ -43,14 +44,17 @@ ax.set_ylabel('X2', color='blue', size=15)
 ax.set_zlabel('Y', color='blue', size=15)
 ax.set_title("Training data as a plane")
 
+# Question 1 b and c - using C = 1 200 and 500
 for C in [1, 200, 5000]:
     # Note that alpha = 1 / C
     model = linear_model.Lasso(alpha=(1/C))
     model.fit(X_poly, y)
     ypred = model.predict(Xtest_poly)
+    # Print model parameters
     print("c = ", C)
     print(model.intercept_)
     print(model.coef_)
+    # Plot model predictions.
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_trisurf(X[:, 0], X[:, 1], y)
@@ -60,15 +64,17 @@ for C in [1, 200, 5000]:
     ax.set_zlabel('Y', color='blue', size=15)
     ax.set_title("Lasso Predictions over [-5, 5] Grid, C = " + str(C))
 
-# Ridge Model
+# Question e - Ridge Model
 for C in [1, 200, 5000]:
     # Note that alpha = 1 / 2C for Ridge regression
     model = Ridge(alpha=(1/(2 * C)))
     model.fit(X_poly, y)
     ypred = model.predict(Xtest_poly)
+    # Print model parameters.
     print("c = ", C)
     print(model.intercept_)
     print(model.coef_)
+    # Plot model predictions.
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(Xtest[:, 0], Xtest[:, 1], ypred, color="red")
--- weekly/3/code/question_2.py
+++ weekly/3/code/question_2.py
@@ -1,13 +1,11 @@
 from sklearn.metrics import mean_squared_error
-from sklearn import linear_model
-from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import KFold
+from sklearn.linear_model import Ridge, Lasso
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import statistics
-from sklearn.linear_model import Ridge
 
 # Read in data
 df = pd.read_csv("week3.csv", comment='#')
@@ -20,49 +18,57 @@ y = np.array(df.iloc[:, 2])
 poly = PolynomialFeatures(5)
 X_poly = poly.fit_transform(X)
 
+# Question a
 # For each fold, keep track of means, variances and folds
 means = []
 variances = []
-folds = []
 C = 1
 all_folds = [2, 5, 10, 25, 50, 100]
 for f in all_folds:
+    # Split the dataset into folds
     kf = KFold(n_splits=f)
     mse = []
+    # Train a model using each fold
     for train, test in kf.split(X_poly):
-        model = linear_model.Lasso(alpha=(1/C))
+        model = Lasso(alpha=(1/C))
         model.fit(X_poly[train], y[train])
         ypred = model.predict(X_poly[test])
+        # Keep the MSE for each model
         mse.append(mean_squared_error(y[test], ypred))
+    # Store the mean, variance for each model
     means.append(statistics.mean(mse))
     variances.append(statistics.variance(mse))
-    folds.append(f)
 
 # Plot the means and variances
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.errorbar(folds, means, label='Mean',
+ax.errorbar(all_folds, means, label='Mean',
             yerr=all_folds, uplims=True, lolims=True)
-ax.errorbar(folds, variances, label='Variance',
+ax.errorbar(all_folds, variances, label='Variance',
             yerr=all_folds, uplims=True, lolims=True)
 ax.set_xlabel("number of folds")
 ax.set_title(
     "Variance and Mean against number of folds used. Lasso Model with C = 1")
 ax.legend(loc='lower right')
 
+# Question b - Lasso model using 10-fold
 means = []
 variances = []
-Cs = []
 num_folds = 10
+# Using a range of Cs between 1 and 75, with a 5 increment per step.
 C_range = np.arange(1, 75, 5)
 for C in C_range:
+    # Split the dataset in 10 folds
     kf = KFold(n_splits=num_folds)
     mse = []
+    # Train a model for each fold
     for train, test in kf.split(X_poly):
-        model = linear_model.Lasso(alpha=(1/C))
+        model = Lasso(alpha=(1/C))
         model.fit(X_poly[train], y[train])
         ypred = model.predict(X_poly[test])
+        # Keep the MSE for each model
         mse.append(mean_squared_error(y[test], ypred))
+    # Store all models folds variances and means.
     means.append(statistics.mean(mse))
     variances.append(statistics.variance(mse))
 
@@ -79,18 +85,22 @@ ax.set_title(
     "Variance and Mean against C value. Lasso Model with folds=10")
 ax.legend(loc='lower right')
 
+# Question d - Ridge model using 10 folds.
 means = []
 variances = []
-Cs = []
 num_folds = 10
 for C in C_range:
+    # Split the dataset in 10 folds
     kf = KFold(n_splits=num_folds)
     mse = []
+    # Train a model for each fold
     for train, test in kf.split(X_poly):
         model = Ridge(alpha=(1/(2 * C)))
         model.fit(X_poly[train], y[train])
         ypred = model.predict(X_poly[test])
+        # Keep the MSE for each model
         mse.append(mean_squared_error(y[test], ypred))
+    # Store all models folds variances and means.
     means.append(statistics.mean(mse))
     variances.append(statistics.variance(mse))
 
@@ -106,6 +116,4 @@ ax.set_xlabel("C")
 ax.set_title(
     "Variance and Mean against C value. Ridge Model with folds=10")
 ax.legend(loc='lower right')
-
-
 plt.show()
