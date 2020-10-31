bb79e17215a83285b4c44db0d0bda1d37bc18a46
sam <petits@tcd.ie>
2020-10-31 20:54:17 +0100

    making report for week 4 - almost done with q i

--- weekly/4/code/qa.py
+++ weekly/4/code/qa.py
@@ -32,18 +32,23 @@ plt.legend((neg, pos), ["positive", "negative"],
 # Compute a simple logistic regression with varying degrees of polynomials.
 degrees = range(1, 10)
 scores = []
+temp = []
 for degree in degrees:
+    # Generate new features
     poly = PolynomialFeatures(degree)
     X_poly = poly.fit_transform(X)
-    model = LogisticRegression().fit(X_poly, y)
+    # Train the model
+    model = LogisticRegression(penalty='l2').fit(X_poly, y)
+    # Keep the score of the model
     scores.append(model.score(X_poly, y))
+    temp.append(mean_squared_error(y, model.predict(X_poly)))
+
 
 # Plot the scores for each model.
 fig = plt.figure()
 plt.rc('font', size=20)
 ax = fig.add_subplot(111)
-ax.errorbar(degrees, scores, label='Mean accuracy',
-            yerr=degrees, uplims=True, lolims=True)
+ax.errorbar(degrees, scores, label='Mean accuracy', yerr=temp)
 ax.set_ylabel("Score (mean accuracy) of the model")
 ax.set_xlabel("Maximum degree of polynomials generated")
 ax.set_title(
@@ -51,10 +56,10 @@ ax.set_title(
 ax.legend(loc='lower right')
 
 # Use interesting degrees to plot the predictions
-for degree in [1, 4, 10]:
+for degree in [1, 2, 10]:
     poly = PolynomialFeatures(degree)
     X_poly = poly.fit_transform(X)
-    model = LogisticRegression(solver='liblinear', penalty='l1').fit(X_poly, y)
+    model = LogisticRegression(solver='liblinear', penalty='l2').fit(X_poly, y)
     y_pred = model.predict(X_poly)
     # Plt the predictions
     fig = plt.figure()
@@ -76,33 +81,33 @@ for degree in [1, 4, 10]:
 # cross validate C value
 C_values = range(1, 30, 1)
 scores = []
+temp = []
 for C in C_values:
-    poly = PolynomialFeatures(4)
+    poly = PolynomialFeatures(2)
     X_poly = poly.fit_transform(X)
-    model = LogisticRegression(
-        solver='liblinear', penalty='l1', C=C).fit(X_poly, y)
+    model = LogisticRegression(C=C).fit(X_poly, y)
     scores.append(model.score(X_poly, y))
+    temp.append(mean_squared_error(y, model.predict(X_poly)))
 
 # Plot the scores for each model.
 fig = plt.figure()
 plt.rc('font', size=20)
 ax = fig.add_subplot(111)
-ax.errorbar(C_values, scores, label='Mean accuracy',
-            yerr=C_values, uplims=True, lolims=True)
+ax.errorbar(C_values, scores, label='Mean accuracy', yerr=temp)
 ax.set_ylabel("Score (mean accuracy) of the model")
 ax.set_xlabel("C")
 ax.set_title(
-    "Cross-validation of the L1 penalty term C for a Logistic Regression model")
+    "Cross-validation of the l2 penalty term C for a Logistic Regression model")
 ax.legend(loc='lower right')
 
 # Plot predictions with some C values
-C_values = [1, 5, 15]
+C_values = [1, 8, 35]
 scores = []
 for C in C_values:
     poly = PolynomialFeatures(4)
     X_poly = poly.fit_transform(X)
     model = LogisticRegression(
-        solver='liblinear', penalty='l1', C=C).fit(X_poly, y)
+        solver='liblinear', penalty='l2', C=C).fit(X_poly, y)
     # Plt the predictions
     fig = plt.figure()
     ax = fig.add_subplot(111)
--- weekly/4/code/qb.py
+++ weekly/4/code/qb.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
 
 # Read in data
 df = pd.read_csv("week4.csv", comment='#')
@@ -13,19 +14,20 @@ y = np.array(df.iloc[:, 2])
 
 neighbors_range = range(1, 20, 1)
 scores = []
+temp = []
 for n in neighbors_range:
     model = KNeighborsClassifier(
         n_neighbors=n, weights='uniform').fit(X, y)
     scores.append(model.score(X, y))
+    temp.append(mean_squared_error(y, model.predict(X)))
 
 # Plot the scores for each model.
 fig = plt.figure()
 plt.rc('font', size=20)
 ax = fig.add_subplot(111)
-ax.errorbar(neighbors_range, scores, label='Mean accuracy',
-            yerr=neighbors_range, uplims=True, lolims=True)
+ax.errorbar(neighbors_range, scores, label='Mean accuracy', yerr=temp)
 ax.set_ylabel("Score (mean accuracy) of the model")
-ax.set_xlabel("neighbors range")
+ax.set_xlabel("k")
 ax.set_title(
     "Cross-validation of the neighbors range for a kNN model")
 ax.legend(loc='lower right')
@@ -57,19 +59,20 @@ for K in K_values:
 # Compute a simple knn model with varying degrees of polynomials.
 degrees = range(1, 50, 2)
 scores = []
+temp = []
 for degree in degrees:
     poly = PolynomialFeatures(degree)
     X_poly = poly.fit_transform(X)
     model = KNeighborsClassifier(
         n_neighbors=K, weights='uniform').fit(X_poly, y)
     scores.append(model.score(X_poly, y))
+    temp.append(mean_squared_error(y, model.predict(X_poly)))
 
 # Plot the scores for each model.
 fig = plt.figure()
 plt.rc('font', size=20)
 ax = fig.add_subplot(111)
-ax.errorbar(degrees, scores, label='Mean accuracy',
-            yerr=degrees, uplims=True, lolims=True)
+ax.errorbar(degrees, scores, label='Mean accuracy', yerr=temp)
 ax.set_ylabel("Score (mean accuracy) of the model")
 ax.set_xlabel("Maximum degree of polynomials generated")
 ax.set_title(
