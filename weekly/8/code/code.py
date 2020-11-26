f43ca25b8ccf4cd8cf1c979dad51edbeca9e16c4
sam <petits@tcd.ie>
2020-11-26 17:21:53 +0100

    report week 8 started

--- weekly/8/code/code.py
+++ weekly/8/code/code.py
@@ -1,7 +1,6 @@
-# from sklearn.kernel_ridge import KernelRidge
-# import matplotlib.pyplot as plt
-# from sklearn.neighbors import KNeighborsRegressor
+from PIL import Image
 import numpy as np
+
 
 def convolutional_layer(image, kernel):
     x_positions = len(image) - len(kernel) + 1
@@ -25,16 +24,15 @@ image = np.array([[1, 2, 3, 4, 5], [1, 3, 2, 3, 10],
                   [3, 2, 1, 4, 5], [6, 1, 1, 2, 2], [3, 2, 1, 5, 4]])
 
 print(convolutional_layer(image, kernel))
- 
-from PIL import Image
+
 im = Image.open('triangle.PNG')
 rgb = np.array(im.convert('RGB'))
 r = rgb[:, :, 0]
-Image.fromarray(np.uint8(r)).show()
+
 kernel1 = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
 kernel2 = np.array([[0, -1, 0], [-1, 8, -1], [0, -1, 0]])
 
 im_k1 = convolutional_layer(r, kernel1)
 im_k2 = convolutional_layer(r, kernel2)
-Image.fromarray(np.uint8(im_k1)).show()
+# Image.fromarray(np.uint8(im_k1)).show()
 Image.fromarray(np.uint8(im_k2)).show()
