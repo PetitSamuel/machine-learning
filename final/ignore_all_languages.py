bc955c20996f7f2e4bf2c8b9fc5f522dcfaae25c
samuel <petits@tcd.ie>
2021-01-04 13:57:50 +0100

    starting point for ignoring language approach

--- final/ignore_all_languages.py
+++ final/ignore_all_languages.py
@@ -1,11 +1,15 @@
+from sklearn.utils import shuffle
 from nltk.stem import PorterStemmer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import KFold
 from sklearn.feature_extraction.text import CountVectorizer
 import json_lines
 import numpy as np
-from sklearn.metrics import mean_squared_error, accuracy_score
+from sklearn.svm import SVC
+from sklearn.linear_model import RidgeClassifier, LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, mean_squared_error, precision_recall_fscore_support
 from sklearn.dummy import DummyClassifier
 
 X = []
@@ -17,15 +21,18 @@ with open('final_data', 'rb') as f:
         y.append(item['voted_up'])
         z.append(item['early_access'])
 
-X = X[:1000]
-y = y[:1000]
-z = z[:1000]
+# Dataset has first 5000 reviews positive and next 5000 negative, shuffle values in unison
+X, y, z = shuffle(X, y, z, random_state=0)
 
-print(len(X))
+X = X[:500]
+y = y[:500]
+z = z[:500]
+
 stemmer = PorterStemmer()
 analyzer = CountVectorizer().build_analyzer()
 
 
+# Returns the stems from words
 def stemmed_words(doc):
     return (stemmer.stem(w) for w in analyzer(doc))
 
@@ -36,12 +43,6 @@ tokens = stem_vectorizer.fit_transform(X)
 # Using stems reduces number of words from 177883 to 44853
 print(len(stem_vectorizer.get_feature_names()))
 
-# print("lets get it")
-# knn_model = KNeighborsClassifier(
-#     n_neighbors=1).fit(X, y)
-# print("lets get it")
-# print(knn_model.score(X, y))
-
 X = np.array(tokens.toarray())
 y = np.array(y)
 
@@ -49,9 +50,16 @@ y = np.array(y)
 kf = KFold(n_splits=5)
 for train, test in kf.split(X):
     print("Going through fold")
-    # LinearRegression().fit(X[train], y[train])
-    model = KNeighborsClassifier().fit(X[train], y[train])
+
+    # KNeighborsClassifier().fit(X[train], y[train])
+    model = LogisticRegression().fit(X[train], y[train])
     ypred = model.predict(X[test])
     print("intercept % f, slope % f, square error % f")
     acc = accuracy_score(y[test], ypred)
     print(acc)
+
+dummy = DummyClassifier(strategy="most_frequent")
+dummy.fit(X, y)
+ypred_dummy = dummy.predict(X)
+acc = accuracy_score(y, ypred_dummy)
+print("dummy: ", acc)
